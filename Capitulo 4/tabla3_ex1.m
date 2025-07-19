47395fa3dc01b2bb543b37b42278d470bc1bc367
Edison <54451309+e-choque@users.noreply.github.com>
2025-07-19 00:03:51 -0400

    Add files via upload

---
+++ Capitulo 4/tabla3_ex1.m
@@ -0,0 +1,17 @@
+% Genera los promedios para la Tabla 3 de la Extensión 1
+
+clear; clc;
+load monte_carlo_ex1_results.mat
+
+m = @(x) mean(x(~isnan(x)));
+
+fprintf('\\begin{tabular}{lcc}\\toprule\n');
+fprintf('Estadística & Promedio\\\\\\midrule\n');
+fprintf('$\\sigma_y$ & %.2f\\\\\n',     m(sigma_y));
+fprintf('$\\sigma_c/\\sigma_y$ & %.2f\\\\\n', m(sigma_c_y));
+fprintf('$\\sigma_i/\\sigma_y$ & %.2f\\\\\n', m(sigma_i_y));
+fprintf('$\\sigma_h/\\sigma_y$ & %.2f\\\\\n', m(sigma_h_y));
+fprintf('$\\sigma_w/\\sigma_y$ & %.2f\\\\\n', m(sigma_w_y));
+fprintf('$\\sigma_h/\\sigma_w$ & %.2f\\\\\n', m(sigma_h_w));
+fprintf('$\\text{corr}(h,w)$ & %.2f\\\\\n',   m(corr_hw));
+fprintf('\\bottomrule\n\\end{tabular}\n');
